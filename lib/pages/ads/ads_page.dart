02c6c23357c77b756437915de3292be167275726
flutterdeveloper7070 <118411934+flutterdeveloper7070@users.noreply.github.com>
2024-11-27 08:14:40 +0530

    some bugs fix

--- lib/pages/ads/ads_page.dart
+++ lib/pages/ads/ads_page.dart
@@ -1,5 +1,7 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class AdsPage extends StatefulWidget {
   const AdsPage({super.key});
@@ -9,15 +11,39 @@ class AdsPage extends StatefulWidget {
 }
 
 class _AdsPageState extends State<AdsPage> {
+  final GlobalKey webViewKey = GlobalKey();
+  InAppWebViewSettings settings = InAppWebViewSettings(
+      isInspectable: kDebugMode,
+      mediaPlaybackRequiresUserGesture: false,
+      allowsInlineMediaPlayback: true,
+      iframeAllow: "camera; microphone",
+      iframeAllowFullscreen: true);
+
   @override
   Widget build(BuildContext context) {
     return WillPopScope(
       onWillPop: () async => false,
       child: Scaffold(
         body: InAppWebView(
+          key: webViewKey,
           initialUrlRequest: URLRequest(
             url: WebUri('http://752.play.online.thopgames.in'),
           ),
+          initialSettings: settings,
+          shouldOverrideUrlLoading: (controller, navigationAction) async {
+            var uri = navigationAction.request.url!;
+
+            if (!["http", "https", "file", "chrome", "data", "javascript", "about"].contains(uri.scheme)) {
+              if (await canLaunchUrl(uri)) {
+                // Launch the App
+                await launchUrl(uri);
+                // and cancel the request
+                return NavigationActionPolicy.CANCEL;
+              }
+            }
+
+            return NavigationActionPolicy.ALLOW;
+          },
         ),
       ),
     );
